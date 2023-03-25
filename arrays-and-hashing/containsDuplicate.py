75d0d675e1bbd674930dfbec000ff42e19fab624
Andy Tran <54690164+aktran321@users.noreply.github.com>
2023-03-25 16:41:24 -0400

    tests
    auto generated some tests using genie

--- arrays-and-hashing/containsDuplicate.py
+++ arrays-and-hashing/containsDuplicate.py
@@ -1,15 +1,23 @@
-# Given an integer array "nums", return true if any value appears twice, false if every element is distinct
-nums = [1,2,3,4]
 def duplicate(nums):
-  #brute force is pick a number, check all other numbers, keep looping until you reach the end or
-  # you find a match... O(n^2) time
-
-  # or make a hash table... 
   hashset = set()
-  length = len(nums)
   for i in nums:
     if i in hashset:
       return True
     else:
-      hashset.add(nums[i])
+      hashset.add(i)
   return False
+
+
+import unittest
+
+class TestDuplicate(unittest.TestCase):
+    def test_duplicate_true(self):
+        nums = [1,2,3,4,4]
+        self.assertTrue(duplicate(nums))
+
+    def test_duplicate_false(self):
+        nums = [1,2,3,4]
+        self.assertFalse(duplicate(nums))
+
+if __name__ == '__main__':
+    unittest.main()
