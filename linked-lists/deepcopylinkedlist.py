c951b79628f70dde1d412a77108d934cd7e56e11
Andy Tran <54690164+aktran321@users.noreply.github.com>
2023-04-09 18:51:33 -0400

    Update deepcopylinkedlist.py

--- linked-lists/deepcopylinkedlist.py
+++ linked-lists/deepcopylinkedlist.py
@@ -4,18 +4,15 @@ class Node:
         self.val = int(x)
         self.next = next
         self.random = random
-            
+# need to make sure, that I am paying closer attention to while loop conditions
 def copyRandomList(head):
         cur = head
-        # ok, what i want to do is loop through the input list, and create new nodes. The new nodes need to be entered into a hashmap. we will use a dictionary.
         ogToCopy = {None:None}
         while cur:
-            newNode = ListNode(cur.val)
+            newNode = Node(cur.val)
             ogToCopy[cur] = newNode
             cur = cur.next
         cur = head
-        # now that we have the newNodes, and a way to access them and change their values
-        # we can loop through the original list again, and then create the links for the newNodes. 
         while cur:
             copy = ogToCopy[cur]
             copy.next = ogToCopy[cur.next]
